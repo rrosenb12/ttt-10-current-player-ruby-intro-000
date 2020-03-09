9fc9166b54a5b037c3df770bdfe2e171b21970f3
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-09 06:31:15 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,10 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-occupied_positions = 0
 def turn_count(board)
-  board.each do
-    board.count do |turn|
-      while occupied_positions <= 9
-        Puts "This is turn number #{turn}"
-        occupied_positions += 1
-      end
+  turns = 0
+  board.each do |space|
+    if space == "" || space == " "
+      turns += 1
     end
   end
+  return turns
 end

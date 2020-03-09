aaac09dad89cfe41dee8d86aee1a39b901ad873a
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-09 06:17:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,9 +4,9 @@ occupied_positions = 0
 def turn_count(board)
   board.each do
     board.count do |turn|
-      while turn <= 9
+      while occupied_positions <= 9
         Puts "This is turn number #{turn}"
-        turn += 1
+        occupied_positions += 1
       end
     end
   end
